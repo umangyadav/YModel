a955047a6ab4b646a50e2e5799dc57a60ea7ac77
umangyadav <umayadav@amd.com>
2023-07-17 13:58:45 +0000

    Add URL option as an argument, modify readme

--- download_uif_models.py
+++ download_uif_models.py
@@ -4,57 +4,64 @@ import subprocess
 import sys
 from zipfile import ZipFile
 import tarfile
-
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-
-install("wget")
-install("beautifulsoup4")
-install("requests")
-
+import argparse as ap
 import requests
 import wget
 from bs4 import BeautifulSoup
 
-url = 'http://mklnxmzoo.amd.com/modelzoo/UIF/1.1-release-migraphx/'
-reqs = requests.get(url, allow_redirects=True)
-soup = BeautifulSoup(reqs.text, 'html.parser')
-curr_dir = os.getcwd()
-dest_path = os.path.join(curr_dir, "YModelTesting")
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-if not os.path.exists(dest_path):
-    os.makedirs(dest_path, exist_ok=True)
+def parse_args():
+    parser = ap.ArgumentParser()
+    parser.add_argument('--modelzoo-url', type='str', help="ModelZoo URL from which, models would be downlaoded")
+    args = parser.parse_args()
+    return args
 
-urls = []
-for link in soup.find_all('a'):
-    file_name = str(link.get('href'))
-    if (file_name.startswith("tf") or file_name.startswith("pt")):
-        if (not re.search("MI100", file_name)
-                and not re.search("MI210", file_name)
-                and not re.search("M100", file_name)
-                and not re.search("M210", file_name)):
-            print("\n Downloading now : ", file_name)
-            file_url = url + '/' + file_name
-            wget.download(file_url, dest_path)
-            file_path = os.path.join(dest_path, file_name)
-            if file_name.endswith(".zip"):
-                try:
-                    with ZipFile(file_path, 'r') as zObject:
-                        zObject.extractall(dest_path)
-                except:
-                    print("\n Bad file detected :", file_name)
-            elif file_name.endswith(".tar") or file_name.endswith(".tar.gz"):
-                try:
-                    tObject = tarfile.open(file_path)
-                    tObject.extractall(path=dest_path)
-                    tObject.close()
-                except:
-                    print("\n Bad file detected: ", file_name)
-            # delete .zip/.tar files after download
-            os.remove(file_path)
-    else:
-        print("\nSkipping downloading :", file_name)
+if __name__ == "__main__":
+    # install necessary packages to crawl website and look for links to download from
+    install("wget")
+    install("beautifulsoup4")
+    install("requests")
+    # parse args
+    args = parse_args() 
+    url = args.modelzoo_url 
+    reqs = requests.get(url, allow_redirects=True)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+    curr_dir = os.getcwd()
+    dest_path = os.path.join(curr_dir, "YModelTesting")
 
-print("\nFinished Downloading and extracting model files.")
+    if not os.path.exists(dest_path):
+        os.makedirs(dest_path, exist_ok=True)
+
+    urls = []
+    for link in soup.find_all('a'):
+        file_name = str(link.get('href'))
+        if (file_name.startswith("tf") or file_name.startswith("pt")):
+            if (not re.search("MI100", file_name)
+                    and not re.search("MI210", file_name)
+                    and not re.search("M100", file_name)
+                    and not re.search("M210", file_name)):
+                print("\n Downloading now : ", file_name)
+                file_url = url + '/' + file_name
+                wget.download(file_url, dest_path)
+                file_path = os.path.join(dest_path, file_name)
+                if file_name.endswith(".zip"):
+                    try:
+                        with ZipFile(file_path, 'r') as zObject:
+                            zObject.extractall(dest_path)
+                    except:
+                        print("\n Bad file detected :", file_name)
+                elif file_name.endswith(".tar") or file_name.endswith(".tar.gz"):
+                    try:
+                        tObject = tarfile.open(file_path)
+                        tObject.extractall(path=dest_path)
+                        tObject.close()
+                    except:
+                        print("\n Bad file detected: ", file_name)
+                # delete .zip/.tar files after download
+                os.remove(file_path)
+        else:
+            print("\nSkipping downloading :", file_name)
+
+    print("\nFinished Downloading and extracting model files.")
